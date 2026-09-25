07791e3818b6004a73e701ae03590368f7708861
vendittellijob2025-byte <vendittellijob2025@gmail.com>
2026-09-25 16:25:23 -0500

    Update app-cj-loader.js

--- app-cj-loader.js
+++ app-cj-loader.js
@@ -1,11 +1,6 @@
-import { NETWORKS } from "./scripts/network-config.mjs";
-
 (function () {
   const CATALOG_URL = "./data/catalog.json";
-
-  const ENABLED_NETWORKS = Object.values(NETWORKS)
-    .filter(network => network.enabled === true)
-    .map(network => network.network);
+  const ENABLED_NETWORKS = ["CJ Affiliate"];
   function normalizeProduct(product, index) {
     const regularPrice =
       typeof product.price === "number"
